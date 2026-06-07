6b19ad20a2b190a8ba7d7e959436145fa6354cb9
tan-zhuo <tanzhuo@aliyun.com>
2026-06-07 16:02:45 +0900

    chore: bump pluginVersion to v1.6.3

--- plugin_meta.go
+++ plugin_meta.go
@@ -3,7 +3,7 @@ package eonId
 // Plugin metadata
 const (
 	PluginName        = "eon-id"
-	PluginVersion     = "v1.6.1"
+	PluginVersion     = "v1.6.3"
 	PluginDescription = "Eon-ID generator plugin with clock drift protection and Redis-based worker ID management"
 	ConfPrefix        = "lynx.eon-id"
 )
